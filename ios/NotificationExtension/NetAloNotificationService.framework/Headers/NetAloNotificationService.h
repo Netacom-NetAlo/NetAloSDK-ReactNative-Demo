d1c82324c9778c3b30b7737837d100842eaa8566
Phong <ttphong2608@gmail.com>
2021-08-17 14:46:36 +0700

    config push with NetAlo

---
+++ ios/NotificationExtension/NetAloNotificationService.framework/Headers/NetAloNotificationService.h
@@ -0,0 +1,18 @@
+//
+//  NetAloNotificationService.h
+//  NetAloNotificationService
+//
+//  Created by Tran Phong on 8/16/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetAloNotificationService.
+FOUNDATION_EXPORT double NetAloNotificationServiceVersionNumber;
+
+//! Project version string for NetAloNotificationService.
+FOUNDATION_EXPORT const unsigned char NetAloNotificationServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetAloNotificationService/PublicHeader.h>
+
+
---
+++ ios/NotificationExtension/NotificationService.swift
@@ -0,0 +1,43 @@
+//
+//  NotificationService.swift
+//  NotificationExtension
+//
+//  Created by Tran Phong on 8/16/21.
+//
+
+import UserNotifications
+import NetAloNotificationService
+
+class NotificationService: UNNotificationServiceExtension {
+
+    var contentHandler: ((UNNotificationContent) -> Void)?
+    var bestAttemptContent: UNMutableNotificationContent?
+  
+    private lazy var notificationService = NetAloNotificationService(environment: .dev, appGroupIdentifier: BuildConfig.default.appGroupIdentifier)
+  
+    override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+      if let bestAttemptContent = self.bestAttemptContent {
+          notificationService.didReceive(bestAttemptContent) { (replaceContent) in
+              if let replaceContent = replaceContent {
+                  contentHandler(replaceContent)
+              } else {
+                  contentHandler(bestAttemptContent)
+              }
+          }
+      } else {
+          contentHandler(request.content)
+      }
+    }
+    
+    override func serviceExtensionTimeWillExpire() {
+        // Called just before the extension will be terminated by the system.
+        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
+        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+
+}
