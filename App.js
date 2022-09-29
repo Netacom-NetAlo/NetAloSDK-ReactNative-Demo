e32237a839e6c6f711b38124a8a49a94b2244ecc
ToanMobile <hvtoan.dev@gmail.com>
2022-09-29 11:08:01 +0700

    no message

--- App.js
+++ App.js
@@ -1,112 +1,156 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
-export default App;
+ import React, { Component, useState } from "react";
+ import ReactNative from "react-native";
+ import {
+   StyleSheet,
+   Text,
+   SafeAreaView,
+   View,
+   Button,
+   TouchableOpacity,
+ } from "react-native";
+ const { NetAloSDK } = ReactNative.NativeModules;
+ 
+ const App = () => {
+   const [shouldShowA, setShouldShowA] = useState(false);
+   const [shouldShowB, setShouldShowB] = useState(false);
+   return (
+     <SafeAreaView style={{ flex: 1 }}>
+       <View style={styles.container}>
+         <Text style={styles.bigtitle}>NetAloSDK Demo React Native</Text>
+         <View style={styles.marginTitle} />
+         {!shouldShowA && !shouldShowB ? (
+           <View>
+             <Text>NetAloSDK Demo Application, Please select user:</Text>
+             <View style={styles.marginButton} />
+             <TouchableOpacity
+               style={[styles.button]}
+               onPress={() => {
+                 NetAloSDK.setUser(
+                   "281474977755108",
+                   "9a0c2c258c4edb30ce63fa4c56070a681464e5d8",
+                   "a6hIg_MRfWKSPeAXkkxAjA6coypt1y6j1KtJAkbd9k_E2w46wZuU4mbhNvA4Uzdl",
+                   "XX",
+                   "+84969143732",
+                   false
+                 );
+                 setShouldShowB(!shouldShowB);
+               }}
+             >
+               <Text style={styles.title}>Set User A</Text>
+             </TouchableOpacity>
+ 
+             <View style={styles.margin} />
+             <TouchableOpacity
+               style={[styles.button]}
+               onPress={() => {
+                 NetAloSDK.setUser(
+                   "281474977755109",
+                   "1f7a20742ef7c7a92decfb27fc9c465d1e6fc8bd",
+                   "a6hIg_MRfWKSPeAXkkxAjA6coypt1y6j1KtJAkbd9k_E2w46wZuU4mbhNvA4Uzdl",
+                   "XX",
+                   "+84969143732",
+                   true
+                 );
+                 setShouldShowA(!shouldShowA);
+               }}
+             >
+               <Text style={styles.title}>Set User B</Text>
+             </TouchableOpacity>
+           </View>
+         ) : null}
+ 
+         <View style={styles.margin} />
+ 
+         {shouldShowA || shouldShowB ? (
+           <View>
+             <TouchableOpacity
+               style={[styles.button]}
+               onPress={() => NetAloSDK.showListConversations()}
+             >
+               <Text style={styles.title}>Show List Conversations</Text>
+             </TouchableOpacity>
+             <View style={styles.margin} />
+           </View>
+         ) : null}
+ 
+         {shouldShowA ? (
+           <TouchableOpacity
+             style={[styles.button]}
+             onPress={() =>
+               NetAloSDK.openChatWithUser(
+                 "281474977755108",
+                 "9a0c2c258c4edb30ce63fa4c56070a681464e5d8"
+               )
+             }
+           >
+             <Text style={styles.title}>Open Chat With User</Text>
+           </TouchableOpacity>
+         ) : null}
+ 
+         {shouldShowB ? (
+           <TouchableOpacity
+             style={[styles.button]}
+             onPress={() =>
+               NetAloSDK.openChatWithUser(
+                 "281474977755109",
+                 "1f7a20742ef7c7a92decfb27fc9c465d1e6fc8bd"
+               )
+             }
+           >
+             <Text style={styles.title}>Open Chat With User</Text>
+           </TouchableOpacity>
+         ) : null}
+       </View>
+     </SafeAreaView>
+   );
+ };
+ 
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     backgroundColor: "#fff",
+     alignItems: "center",
+     justifyContent: "center",
+   },
+   input: {
+     height: 50,
+     padding: 10,
+     margin: 18,
+     fontSize: 14,
+     borderWidth: 1,
+     borderRadius: 10,
+     borderColor: "#ff9900",
+     backgroundColor: "rgba(0,0,0,0)",
+   },
+   button: {
+     fontSize: 22,
+     height: 50,
+     backgroundColor: "#ff9900",
+     borderRadius: 10,
+     paddingHorizontal: 20,
+     alignItems: "center",
+     justifyContent: "center",
+   },
+   title: {
+     textAlign: "center",
+     fontSize: 22,
+     color: "#fff",
+   },
+   bigtitle: {
+     fontSize: 30,
+     color: "#ff9900",
+     textAlign: "center",
+   },
+   margin: {
+     height: 20,
+   },
+   marginTitle: {
+     height: 100,
+   },
+   marginButton: {
+     height: 10,
+   },
+ });
+ 
+ export default App;
+ 
